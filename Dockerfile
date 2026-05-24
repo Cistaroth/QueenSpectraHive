40c5650c49429f46cbb7a6fd82ecc019b6753cca
Cistaroth <t.h.a.luu@student.rug.nl>
2026-05-24 15:50:11 +0200

    Added dockerization of program

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ghcr.io/astral-sh/uv:python3.12-bookworm-slim
+
+WORKDIR /app
+
+COPY pyproject.toml uv.lock ./
+RUN uv sync --frozen --no-install-project --no-dev
+
+COPY . .
+RUN uv sync --frozen --no-dev
+
+CMD ["uv", "run", "src/main.py"]
--- src/config.py
+++ src/config.py
@@ -3,14 +3,16 @@ from pathlib import Path
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-ENV_FILEPATH = Path(__file__).parent / ".env"
+ENV_FILEPATH = Path(__file__).parents[1] / ".env"
+
 
 class Config(BaseSettings):
     """
     Configurations for the application
     """
+
     model_config = SettingsConfigDict(
-        env_file= ENV_FILEPATH,    
+        env_file=ENV_FILEPATH,
         env_file_encoding="utf-8",
         extra="ignore",
     )
@@ -25,6 +27,7 @@ class Config(BaseSettings):
     API_DESCRIPTION: str
     API_VERSION: str
     KAGGLE_API_TOKEN: str
+
 
 load_dotenv(ENV_FILEPATH)
 config = Config()
